c0edf4d69281f0371ed8ee3b20e9d84b6a713ace
kraneelManandhar <mydarling3547@gmail.com>
2025-07-24 22:13:36 +0545

    JWT again

--- dayTwelve_to_/dayTwelve.js
+++ dayTwelve_to_/dayTwelve.js
@@ -9,30 +9,49 @@ const  schema = require('./models/Schema');
 app.use(express.json());
 
 
+const jwt_test = require('./jwt');
+app.use('/jwt',jwt_test);
+
 const smth = require('./Routes/smth');
 
 app.use('/smth',smth);
+
 
 mongoose.connect('mongodb://localhost:27017/taskTracker')
 .then(()=> console.log("Connected to database"))
 .catch(()=>console.log("Error connecting to database"))
 
-app.get('/tasks',async (req,res) => {
-    const tasks = await schema.find();
-    console.log(tasks);
-    res.send(tasks);
+
+app.get('/posts',(req,res) => {
+    res.json(req.body)
+    console.log(req.body);
 })
 
-app.post('/post',(req,res) => {
-    try{
-    const data = req.body;
-    const Userdata = new schema(data);
-    Userdata.save();
-    res.send("Task saved successfully")
-    }catch(error){
-        res.send("Error saving tasks")
-    }
+app.get('/login',(req,res) =>{
+
+    const username = req.body.name;
+    
+
+    jwt.sign({name : username}, process.env.ACCESS_TOKEN_SECRET)
 })
+
+
+// app.get('/tasks',async (req,res) => {
+//     const tasks = await schema.find();
+//     console.log(tasks);
+//     res.send(tasks);
+// })
+
+// app.post('/post',(req,res) => {
+//     try{
+//     const data = req.body;
+//     const Userdata = new schema(data);
+//     Userdata.save();
+//     res.send("Task saved successfully")
+//     }catch(error){
+//         res.send("Error saving tasks")
+//     }
+// })
 
 
 app.listen(port,() => {
--- dayTwelve_to_/jwt.js
+++ dayTwelve_to_/jwt.js
@@ -1,1 +1,43 @@
 const jwt = require('jsonwebtoken');
+const express = require('express');
+const app = express();
+require('dotenv').config()
+app.use(express.json());
+
+app.get('/posts',authenticate,(req,res) => {
+    // const user = {
+    //     "name" : "Nikka",
+    //     "task" : "Be The Sun God"
+    // }
+    // res.json(user)
+
+    res.json(postMessage.filter(post => post.username === req.user.name))
+})
+
+app.get('/login',(req,res) =>{
+
+    const username = req.body.name;
+    const user = { name : username}
+    
+
+    const accessToken =  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken : accessToken})
+})
+
+function authenticate(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token ==null){
+        return res.sendStatus(401)
+    }
+
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET , (err,user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user;
+        next();
+    })
+}
+
+app.listen(5000,()=>{
+    console.log("Hosted at http://localhost:5000");
+})
