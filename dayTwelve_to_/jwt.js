1117703683d46c28b7daddf3b42d01b91023bdf3
kraneelManandhar <mydarling3547@gmail.com>
2025-07-25 22:14:56 +0545

    Again jwt

--- dayTwelve_to_/jwt.js
+++ dayTwelve_to_/jwt.js
@@ -4,39 +4,38 @@ const app = express();
 require('dotenv').config()
 app.use(express.json());
 
-app.get('/posts',authenticate,(req,res) => {
-    // const user = {
-    //     "name" : "Nikka",
-    //     "task" : "Be The Sun God"
-    // }
-    // res.json(user)
+const postMessage = [
+    {username: "Kraneel",task: "Code JWT system" },
+    {username: "Nikka",task: "Be The Sun God" },
+    {username: "Kraneel",task: "Study Node.js" }
+];
 
-    res.json(postMessage.filter(post => post.username === req.user.name))
-})
-
-app.get('/login',(req,res) =>{
-
+app.post('/login', (req, res) => {
     const username = req.body.name;
-    const user = { name : username}
-    
+    if (!username) return res.status(400).json({ message: "Name required" });
 
-    const accessToken =  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({accessToken : accessToken})
-})
+    const user = { name: username };
 
-function authenticate(req,res,next){
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-    if (token ==null){
-        return res.sendStatus(401)
-    }
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+    res.json({ accessToken: accessToken });
+});
 
-    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET , (err,user) => {
-        if (err) return res.sendStatus(403)
+function authenticate(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) return res.sendStatus(401);
+     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+
         req.user = user;
         next();
-    })
+    });
 }
+app.get('/posts', authenticate, (req, res) => {
+    const userPosts = postMessage.filter(post => post.username === req.user.name);
+    res.json(userPosts);
+});
 
 app.listen(5000,()=>{
     console.log("Hosted at http://localhost:5000");
